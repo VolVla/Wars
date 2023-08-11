8ca65d999b201b7d324fc5eae6d4afddfffedd87
VolVla <98841869+VolVla@users.noreply.github.com>
2023-08-11 15:25:31 +0300

    updateupdate2

--- Wars/Program.cs
+++ Wars/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Wars
 {
@@ -13,14 +12,11 @@ namespace Wars
 
             while (isWork)
             {
-                World battleArena = new World();
-
-                battleArena.CreateCountry();
-                battleArena.CreateCountry();
-                Console.ReadLine();
+                Battlefild battlefild = new Battlefild();
                 Console.WriteLine("Для начало сражения нажмите на любую клавишу");
                 Console.ReadKey();
-                Console.Clear();
+
+                battlefild.Battle();
 
                 Console.WriteLine($"\nВы хотите выйти из программы?Нажмите {_exitButton}.\nДля продолжение работы нажмите любую другую клавишу");
 
@@ -42,91 +38,99 @@ namespace Wars
         void Attack(Unit warrior, List<Unit> targets);
     }
 
-    class World
-    {
-        private List<Country> _countries = new List<Country>();
-
-        public string NameCountry { get; private set; }
-
-        public void CreateCountry()
-        {
-            Console.WriteLine("Название страны");
-            NameCountry = Console.ReadLine();
-            _countries.Add(new Country(NameCountry));
-            CreateValueSquad();
-        }
-
-        private void CreateValueSquad()
-        {
-            foreach (Country country in _countries)
-            {
-                Console.WriteLine("Введите количество солдат в отряде");
-                int.TryParse(Console.ReadLine(), out int numberUnit);
-                country.CreateSquad(numberUnit);
-
-                foreach (var squad in country.GetSquad())
-                {
-                    squad.CreateSoldiers(numberUnit, country.Name);
-                    squad.ShowSoldiers();
-                }
-            }
-
-            War war = new War(_countries);
-            war.Start();
-        }
-    }
-
     class Country
     {
-        private List<Squad> _squad = new List<Squad>();
+        private Squad _squad;
+
+        public Country()
+        {
+            Console.WriteLine("Введите название страны");
+            Name = Console.ReadLine();
+            Console.WriteLine("Введите количество солдат");
+            int.TryParse(Console.ReadLine(), out int numberSoldiers);
+            _squad = new Squad(numberSoldiers, Name);
+        }
 
         public string Name { get; }
 
-        public Country(string name)
+        public Squad GetSquad()
         {
-            Name = name;
+            return _squad;
+        }
+    }
+
+    class Battlefild
+    {
+        private int _numberFirstCountry = 0;
+        private int _numberSecondCountry = 1;
+        private List<Country> _countries = new List<Country>()
+        {
+             new Country(),
+             new Country(),
+        };
+
+        public void Battle()
+        {
+            while (_countries[_numberFirstCountry].GetSquad().IsDeadSquad != true && _countries[_numberSecondCountry].GetSquad().IsDeadSquad != true)
+            {
+                _countries[_numberFirstCountry].GetSquad().AttackSquad(_countries[_numberSecondCountry].GetSquad());
+                _countries[_numberSecondCountry].GetSquad().AttackSquad(_countries[_numberFirstCountry].GetSquad());
+            }
+
+            ResultBattle();
         }
 
-        public void CreateSquad(int number)
+        private void ResultBattle()
         {
-            _squad.Add(new Squad(number));
-        }
-
-        public List<Squad> GetSquad()
-        {
-            return _squad.ToList<Squad>();
+            if (_countries[_numberFirstCountry].GetSquad().IsDeadSquad == true & _countries[_numberSecondCountry].GetSquad().IsDeadSquad == true)
+            {
+                Console.WriteLine("Отряды по убивали друг друга, никто не победил");
+            }
+            else if (_countries[_numberFirstCountry].GetSquad().IsDeadSquad == false & _countries[_numberSecondCountry].GetSquad().IsDeadSquad == true)
+            {
+                Console.WriteLine($"Победила страна {_countries[_numberFirstCountry].Name}");
+            }
+            else if (_countries[_numberFirstCountry].GetSquad().IsDeadSquad == true & _countries[_numberSecondCountry].GetSquad().IsDeadSquad == false)
+            {
+                Console.WriteLine($"Победила страна {_countries[_numberSecondCountry].Name}");
+            }
         }
     }
 
     class Squad
     {
+        public bool IsDeadSquad = false;
         private List<Unit> _soldiers = new List<Unit>();
         private List<IWeapon> _weapons = new List<IWeapon>() { new Rifle(50), new MachineGun(20) };
         private Random _random = new Random();
 
-        public Squad(int number)
+        public Squad(int number, string name)
         {
             Number = number;
+            CreateSoldiers(number, name);
+            ShowSoldiers();
         }
 
         public int Number { get; }
-        public int CountWarriors { get { return _soldiers.Count; } }
-        public int MaxCountWarriors { get; }
 
-        public List<Unit> GetSoldiers()
+        public void AttackSquad(Squad squad)
         {
-            return _soldiers.ToList<Unit>();
-        }
+            int numberUnitAttackSquad = _random.Next(0, _soldiers.Count);
 
-        public void CreateSoldiers(int numberSoldiersSquad, string nationality)
-        {
-            for (int i = 1; i <= MaxCountWarriors; i++)
+            for (int i = 0; i < numberUnitAttackSquad; i++)
             {
-                _soldiers.Add(new Unit(i, numberSoldiersSquad, nationality, GiveWeapon()));
+                _soldiers[i].Attack(squad.GetSoldiers());
+            }
+
+            squad.RemoveDeathSoldiers();
+
+            if (_soldiers.Count == 0)
+            {
+                IsDeadSquad = true;
             }
         }
 
-        public void ShowSoldiers()
+        private void ShowSoldiers()
         {
             foreach (var soldier in _soldiers)
             {
@@ -134,9 +138,23 @@ namespace Wars
             }
         }
 
-        public void RemoveDeathSoldiers()
+        private void CreateSoldiers(int numberSoldiersSquad, string nationality)
+        {
+            for (int i = 1; i <= Number; i++)
+            {
+                _soldiers.Add(new Unit(i, numberSoldiersSquad, nationality, GiveWeapon()));
+            }
+        }
+
+        private void RemoveDeathSoldiers()
         {
             _soldiers.RemoveAll(soldier => soldier.Health <= 0);
+            Console.WriteLine("Убрали с поля боя мертвых солдат");
+        }
+
+        private List<Unit> GetSoldiers()
+        {
+            return _soldiers;
         }
 
         private IWeapon GiveWeapon()
@@ -147,107 +165,9 @@ namespace Wars
         }
     }
 
-    class War
-    {
-        private List<Country> _countries = new List<Country>();
-        private List<Battle> _battles = new List<Battle>();
-
-        public War(List<Country> countries)
-        {
-            _countries = countries;
-        }
-
-        public void Start()
-        {
-            for (int i = 1; GetLifeCountries().Count > 1; i++)
-            {
-                Console.WriteLine("Бой " + i);
-                ExecuteBattle();
-            }
-        }
-
-        private void ExecuteBattle()
-        {
-            var battle = new Battle(_countries);
-            _battles.Add(battle);
-            battle.ExecuteThis();
-        }
-
-        private List<Country> GetLifeCountries()
-        {
-            List<Country> lifeCountries = new List<Country>();
-
-            foreach (var country in _countries)
-            {
-                foreach (var soldier in country.GetSquad())
-                {
-                    if (soldier.CountWarriors > 0)
-                    {
-                        lifeCountries.Add(country);
-                    }
-                }
-            }
-
-            return lifeCountries.ToList<Country>();
-        }
-    }
-
-    class Battle
-    {
-        private List<Country> _countries = new List<Country>();
-
-        public Battle(List<Country> countries)
-        {
-            _countries = countries;
-        }
-
-        private List<Unit> GetAllSoldiers()
-        {
-            List<Unit> allSoldiers = new List<Unit>();
-
-            foreach (var country in _countries)
-            {
-                foreach (var platoon in country.GetSquad())
-                {
-                    foreach (var soldier in platoon.GetSoldiers())
-                    {
-                        allSoldiers.Add(soldier);
-                    }
-                }
-            }
-
-            return allSoldiers.ToList<Unit>();
-        }
-
-        public void ExecuteThis()
-        {
-            var allwarriors = GetAllSoldiers();
-
-            foreach (var warrior in GetAllSoldiers())
-            {
-                warrior.Attack(allwarriors);
-            }
-
-            foreach (var country in _countries)
-            {
-                foreach (var squad in country.GetSquad())
-                {
-                    squad.RemoveDeathSoldiers();
-                    Console.WriteLine($"{country.Name} - Потери: {squad.MaxCountWarriors - squad.CountWarriors} Солдат");
-                    squad.ShowSoldiers();
-                }
-            }
-        }
-    }
-
     class Unit
     {
         public int Health;
-
-        public int Number { get; private set; }
-        public int NumberSoldier { get; private set; }
-        public string Nationality { get; private set; }
-        public IWeapon Weapon { get; private set; }
 
         public Unit(int number, int numberPlatoon, string nationality, IWeapon weapon, int health = 100)
         {
@@ -258,9 +178,14 @@ namespace Wars
             Weapon = weapon;
         }
 
+        public int Number { get; private set; }
+        public int NumberSoldier { get; private set; }
+        public string Nationality { get; private set; }
+        public IWeapon Weapon { get; private set; }
+
         public void ShowInfo()
         {
-            Console.WriteLine($"{Nationality}.{NumberSoldier}.{Number}.{Weapon.Name}.{Health}");
+            Console.WriteLine($"Национальность - {Nationality}, номер солдата - {Number}, вооруженме -{Weapon.Name}, здоровье - {Health}");
         }
 
         public void Attack(List<Unit> targets)
@@ -271,55 +196,47 @@ namespace Wars
 
     class Rifle : IWeapon
     {
-        public int Damage;
-        private Random _random = new Random();
+        private int _damage;
 
         public Rifle(int damage)
         {
-            Damage = damage;
+            _damage = damage;
         }
 
         string IWeapon.Name { get { return "Винтовка"; } }
 
         public void Attack(Unit soldier, List<Unit> targets)
         {
-            List<Unit> enemies = new List<Unit>();
-
-            foreach (var target in targets)
+            foreach (Unit target in targets)
             {
-                if (soldier.Nationality != target.Nationality)
+                if (targets != null)
                 {
-                    enemies.Add(target);
+                    target.Health -= _damage;
+                    soldier.ShowInfo();
+                    Console.Write($"Атаковал солдата на - {_damage} - ");
+                    target.ShowInfo();
                 }
-            }
-
-            var targetAttack = enemies[_random.Next(enemies.Count)];
-
-            if (targetAttack != null)
-            {
-                targetAttack.Health -= Damage;
-                soldier.ShowInfo();
-                Console.Write($" - {Damage} - ");
-                targetAttack.ShowInfo();
             }
         }
     }
 
     class MachineGun : IWeapon
     {
-        public int Damage;
-        string IWeapon.Name { get { return "Пулемёт"; } }
+        private int _damage;
 
         public MachineGun(int damage)
         {
-            Damage = damage;
+            _damage = damage;
         }
+
+        string IWeapon.Name { get { return "Пулемёт"; } }
 
         public void Attack(Unit warrior, List<Unit> targets)
         {
-            var rifle = new Rifle(Damage);
+            var rifle = new Rifle(_damage);
+            int maximumTargets = 3;
 
-            for (var i = 0; i < 3; i++)
+            for (var i = 0; i < maximumTargets; i++)
             {
                 rifle.Attack(warrior, targets);
             }
